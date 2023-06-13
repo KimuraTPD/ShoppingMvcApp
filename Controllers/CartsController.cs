c2b49cb54d488a4bdb29e91c8544332d96271bda
ichikawa000 <yu.ichikawa@intra.remtiro.com>
2023-06-13 14:26:31 +0900

    カートの商品削除機能追加

--- Controllers/CartsController.cs
+++ Controllers/CartsController.cs
@@ -58,21 +58,6 @@ namespace ShoppingMvcApp.Controllers
             return View();
         }
 
- //カートサイトに表示用
-        // public void DisplayCartToProduct(int productId, string productName, int price){
-        //     // Sessionからカートを取得
-        //     if(HttpContext.Session.Get("cartList") != null)
-        //     {
-        //         cartList = (List<Product>)BytesToObject(HttpContext.Session.Get("cartList"));
-        //     }
-        //   //  Console.WriteLine("id = " + productId + ", name = " + productName + ", price = " + price);
-
-        //     Product product = new Product(productId, productName, price);
-        //     foreach(Product p in cartList)
-        //     {
-        //         p.showData();
-        //     }
-        // }
         public IActionResult OrderedPage(){
             User user = new User(); 
             user = (User)user.ct(HttpContext.Session.Get("object"));
@@ -80,6 +65,68 @@ namespace ShoppingMvcApp.Controllers
             ViewData["pass"] =  user.password;
             return View("../Carts/Ordered");
         }
+
+         public async Task<IActionResult> DeleteCart(int? id)
+        {
+            User user = new User(); 
+            user = (User)user.ct(HttpContext.Session.Get("object"));
+            ViewData["mail"] = user.mail;
+            ViewData["pass"] =  user.password;
+
+            int id2 = (int)id;
+   
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .FirstOrDefaultAsync(m => m.productId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Sessionからカートを取得
+            if(HttpContext.Session.Get("cartList") != null)
+            {
+                Console.WriteLine("session get");
+                cartList = (List<Product>)BytesToObject(HttpContext.Session.Get("cartList"));
+            }
+            bool check = false;
+            for(int i =0; i< cartList.Count; i++)
+            {
+                // 既にカートに同じ商品が追加されている場合
+                if(cartList[i].productId== product.productId)
+                {
+                    cartList.RemoveAt(i);
+                    Console.WriteLine("cart");
+                }
+            }
+
+            foreach(Product p in cartList)
+            {
+                p.showData();
+            }
+
+            // カートリストをSessionにセット
+            HttpContext.Session.Set("cartList",ObjectToBytes(cartList));
+
+            // カートリストをViewDataにセット
+            ViewData["cartList"] = cartList;
+
+
+            return View("../Carts/index");
+        }
+         public static byte[] ObjectToBytes(Object ob)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+            bf.Serialize(ms, ob);
+            return ms.ToArray();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
